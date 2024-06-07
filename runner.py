c53d9308f983f5a51a2c98c55585d10387132507
Vladimir <vovatakeda@mail.ru>
2024-06-08 00:26:25 +0200

    added github workflow

--- runner.py
+++ runner.py
@@ -1,4 +1,7 @@
+import os
 import subprocess
+import requests
+import argparse
 import numpy as np
 from multiprocessing import shared_memory
 
@@ -18,8 +21,31 @@ except Exception as e:
     print(f"Error: {e}")
     exit(1)
 
-command1 = "./cmake-build-release/Epam-cpp /home/vladimir/Desktop/Epam-cpp/_720_2100169427.mp4 /home/vladimir/Desktop/Epam-cpp/out.mp4 " + SHM_NAME
-command2 = "./cmake-build-release/Epam-cpp /home/vladimir/Desktop/Epam-cpp/_720_2100169427.mp4 /home/vladimir/Desktop/Epam-cpp/out.mp4 " + SHM_NAME
+parser = argparse.ArgumentParser()
+
+parser.add_argument('binary', type=str)
+parser.add_argument('projectDir', type=str)
+
+args = parser.parse_args()
+
+def download_file(url, local_filename):
+    if not os.path.exists(local_filename):
+        with requests.get(url, stream=True) as response:
+            response.raise_for_status()
+            with open(local_filename, 'wb') as file:
+                for chunk in response.iter_content(chunk_size=8192):
+                    file.write(chunk)
+        print(f"Файл был успешно скачан и сохранен как {local_filename}")
+    else:
+        print(f"Файл {local_filename} уже существует, скачивание пропущено.")
+
+url = "https://hd.trn.su/720/2100169427.mp4?md5=h3rzwagskVzQZV2qT1neUQ&time=1717809174&d=1"
+local_filename = "downloaded_video.mp4"
+
+download_file(url, local_filename)
+
+command1 = f"{args.binary} {args.projectDir}/{local_filename} {args.projectDir}/out.mp4 " + SHM_NAME
+command2 = f"{args.binary} {args.projectDir}/{local_filename} {args.projectDir}/out.mp4 " + SHM_NAME
 
 process1 = subprocess.Popen(command1, shell=True)
 process2 = subprocess.Popen(command2, shell=True)
